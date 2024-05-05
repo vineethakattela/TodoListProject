046aa3425e1262beb40f0c021878a47d6f3d0ba3
vineethakattela <165314116+vineethakattela@users.noreply.github.com>
2024-05-05 14:53:01 +0530

    Update Todoinput.jsx

--- src/Components/Todoinput.jsx
+++ src/Components/Todoinput.jsx
@@ -4,7 +4,7 @@ const Todoinput = (props) => {
   const{handleAddTodos,todoValue,setTodoValue}=props
  
   return (
-    <div>
+    <header>
      <input value={todoValue} onChange={(e)=>{
  setTodoValue(e.target.value)
      }} placeholder='Enter todo..' />
@@ -13,7 +13,7 @@ const Todoinput = (props) => {
       handleAddTodos(todoValue)
       setTodoValue('')
      }}>Add</button>
-    </div>
+    </header>
   )
 }
 
